73a5ae3ac00833cf69119979621b951ed0738e67
Gowtham <112604105+Gowtham152003@users.noreply.github.com>
2023-07-09 16:07:55 +0530

    passing values to class.py
    Let's visit beach....
    Thanks for visiting Goa beach here is your drink Coke

---
+++ passing values to class.py
@@ -0,0 +1,12 @@
+#passing values
+class goa:
+    drinks =""
+    def party(self):
+        print("Let's party with Goa...")
+    def beach(self):
+        print("Let's vist beach....")
+
+ramesh =goa()
+ramesh.drinks="Coke"
+ramesh.beach()
+print("Thanks for visiting Goa beach here is your drink",ramesh.drinks)
