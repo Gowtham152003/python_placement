fd4c3db1d27a7094f5109f268045a535c80df230
Gowtham <112604105+Gowtham152003@users.noreply.github.com>
2023-07-09 16:07:01 +0530

    Class creation.py
    Let's visit beach....

---
+++ Class creation.py
@@ -0,0 +1,11 @@
+#class creation - class is a collection of objects.
+class goa:
+    drinks =""
+    def party(self):
+        print("Let's party with Goa...")
+    def beach(self):
+        print("Let's visit beach....")
+
+#attribute or object declaration....
+ramesh =goa()
+ramesh.beach()
