88673d192b3395c73643492b8f8d7cbb901a4321
Gowtham <112604105+Gowtham152003@users.noreply.github.com>
2023-07-09 16:10:02 +0530

    Constructor.py
    Welcome to Goa

---
+++ Constructor.py
@@ -0,0 +1,11 @@
+class goa:
+    drinks =" "
+    def party(self):
+        print("Let's party with Goa...")
+    def beach(self):
+        print("Let's vist beach....")
+    def __init__(self):
+        print("Welcome to Goa")
+
+
+ramesh=goa()
